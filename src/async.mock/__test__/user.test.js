fa858131a01c5a4484a05641d9e4d723322d7a95
crystalfn <969274162@qq.com>
2020-11-15 22:02:43 +0800

    [fangni] feat: implement jest homework

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,23 @@
+import axios from "axios";
+import { verifyUsername } from "../verify";
 import { register } from "../user";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    // return expect(register()).resolves.toBe();
+    const response = {};
+    const data = { data: response };
+    axios.post.mockResolvedValue(data);
+    await expect(register()).resolves.toBe(response);
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+    await expect(register()).rejects.toThrow("wrong username or password");
   });
 });
